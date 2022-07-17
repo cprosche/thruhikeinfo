b0ae89ac43221d5a69d8441b26f9f2e17bf014f8
Cade Rosche <caderosche@gmail.com>
2022-07-17 10:28:21 -0500

    added todos

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
 
+// TODO: add google search console
+// TODO: SEO
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
