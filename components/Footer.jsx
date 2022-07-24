86124632e9baf3e663164d17cb1475fd8d2b61dc
Cade Rosche <caderosche@gmail.com>
2022-07-24 09:37:11 -0500

    added version in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,16 +5,21 @@ const Footer = () => (
   <Container>
     <Row className="py-5 align-items-center">
       <Col lg={3}>
-        <Link href={"https://www.caderosche.dev"}>Cade Rosche</Link> &copy;{" "}
-        {new Date().getFullYear()}
+        <div>
+          <Link href={"https://www.caderosche.dev"}>Cade Rosche</Link> &copy;{" "}
+          {new Date().getFullYear()}
+        </div>
+        <div>v0-alpha</div>
       </Col>
       <Col lg={6} className="text-lg-center">
-        *This data is estimated and should not be relied upon without further research. It is meant as a starting point for hike planning, not as the only information used to plan your thru hike. 
+        *This data is estimated and should not be relied upon without further
+        research. It is meant as a starting point for hike planning, not as the
+        only information used to plan your thru hike.
       </Col>
       <Col lg={3} className="text-lg-end">
-        <Link href={"https://github.com/cprosche"}>
+        <a href="https://github.com/cprosche">
           Soon, this will be open source...
-        </Link>
+        </a>
       </Col>
     </Row>
   </Container>
