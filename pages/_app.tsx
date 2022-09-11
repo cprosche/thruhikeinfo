11daad448fb8bb57dd575c01edff257606ae5bf1
Cade Rosche <caderosche@gmail.com>
2022-09-11 09:12:43 -0500

    only GA in prod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,14 @@ import { useEffect } from "react";
 import "../styles/globals.css";
 import { analytics } from "../utils/analytics";
 
-// TODO: build a hiking budget calculator
 // TODO: add next.js static props and other static site gen components
 // TODO: add page for planned features
 // TODO: add sitemap
 function Application({ Component, pageProps }) {
   useEffect(() => {
-    analytics(window, document, "script", "dataLayer", "GTM-MGJM6LK");
+    if (process.env.NODE_ENV === "production") {
+      analytics(window, document, "script", "dataLayer", "GTM-MGJM6LK");
+    }
   });
   return <Component {...pageProps} />;
 }
