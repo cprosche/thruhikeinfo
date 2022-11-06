f5c10d3f9e351027357473ec0a076abee4a0dede
Cade Rosche <caderosche@gmail.com>
2022-11-06 07:37:55 -0600

    added rtk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
 import "../styles/globals.css";
 import { analytics } from "../utils/analytics";
+import { Provider } from "react-redux";
+import { store } from "../rtk/store";
 
 // TODO: add next.js static props and other static site gen components
 // TODO: add sitemap
@@ -11,7 +13,11 @@ function Application({ Component, pageProps }) {
       analytics(window, document, "script", "dataLayer", "GTM-MGJM6LK");
     }
   });
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default Application;
---
+++ rtk/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../rtk/trailsSlice";
+import { pokemonApi } from "./trailsApi";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
