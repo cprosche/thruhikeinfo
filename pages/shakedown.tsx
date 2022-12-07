4562a6435a5aceffb2e6878e95521d2c0addb1fb
cprosche <caderosche@gmail.com>
2022-12-07 16:32:17 -0600

    added todos for shakedown

---
+++ pages/shakedown.tsx
@@ -0,0 +1,26 @@
+import { Col, Container, Row } from "react-bootstrap";
+import Layout from "../components/layout/Layout";
+
+// TODO: dummy data
+// TODO: create row component
+// TODO: create row group component
+// TODO: add group functionality
+// TODO: add item functionality
+// TODO: create chart component with basic addition
+// TODO: add login
+// TODO: create api
+// TODO: add CRUD to api
+// TODO: add list name
+const Shakedown = () => (
+  <Layout>
+    <Container>
+      <Row>
+        <Col>
+          <div>Shakedown</div>
+        </Col>
+      </Row>
+    </Container>
+  </Layout>
+);
+
+export default Shakedown;
