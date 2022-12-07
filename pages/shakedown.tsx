94efd47e2af527874a3aa5933b2ab8539c615fd9
cprosche <caderosche@gmail.com>
2022-12-07 16:37:29 -0600

    more todos

--- pages/shakedown.tsx
+++ pages/shakedown.tsx
@@ -7,10 +7,13 @@ import Layout from "../components/layout/Layout";
 // TODO: add group functionality
 // TODO: add item functionality
 // TODO: create chart component with basic addition
+// TODO: improve chart component
 // TODO: add login
 // TODO: create api
 // TODO: add CRUD to api
 // TODO: add list name
+// TODO: responsive
+// TODO: add print/export pdf packing list
 const Shakedown = () => (
   <Layout>
     <Container>
